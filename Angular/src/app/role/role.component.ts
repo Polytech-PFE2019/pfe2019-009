c876f63833f54dc11bba9ce2c81bf5614a6bc80b
LiuJiaqi-unice <katherine9677@163.com>
2019-11-20 10:10:10 +0100

    add role page

--- Angular/src/app/role/role.component.ts
+++ Angular/src/app/role/role.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./role.component.css']
 })
 export class RoleComponent implements OnInit {
-
+  index1 = 1;
   constructor() { }
 
   ngOnInit() {

a80afc660e9acfc0c3fdf14015e47478bb422a99
Gilles Chandelier <gilles.chandelier@etu.unice.fr>
2019-12-03 18:03:50 +0100

    fix chat

--- Angular/src/app/service/gameOnService/game-on.service.ts
+++ Angular/src/app/service/gameOnService/game-on.service.ts
@@ -1,12 +1,12 @@
-import {Injectable} from '@angular/core';
-import {WebsocketService} from '../webSocketService/websocket.service';
-import {SubscriptionService} from '../subscriptionSerivce/subscription.service';
-import {Subject} from 'rxjs';
-import {SocketRequest} from '../../../Request';
-import {URLGame} from '../../model/url';
-import {map} from 'rxjs/operators';
-import {Activity} from '../../model/activity';
-import {ActionSet} from '../../model/action';
+import { Injectable } from '@angular/core';
+import { WebsocketService } from '../webSocketService/websocket.service';
+import { SubscriptionService } from '../subscriptionSerivce/subscription.service';
+import { Subject } from 'rxjs';
+import { SocketRequest } from '../../../Request';
+import { URLGame } from '../../model/url';
+import { map } from 'rxjs/operators';
+import { Activity } from '../../model/activity';
+import { ActionSet } from '../../model/action';
 
 @Injectable()
 export class GameOnService {
@@ -24,15 +24,9 @@ export class GameOnService {
   current: any;
   history = new Subject<any>();
   history$ = this.history.asObservable();
-  minCost = 0;
-  maxCost = 0;
-  minTime = 0;
-  maxTime = 0;
-  minFailure = 0;
-  maxFailure = 0;
 
   constructor(private wsService: WebsocketService,
-              private subscription: SubscriptionService) {
+    private subscription: SubscriptionService) {
     this.messages = wsService
       .connect(URLGame)
       .pipe(
@@ -41,13 +35,6 @@ export class GameOnService {
           console.log(data);
           if (JSON.stringify(data).includes('players')) {
 
-          }
-          if (JSON.stringify(data).includes('userID')) {
-            if (data.userID !== undefined) {
-              console.log('send userID');
-              this.userID = data.userID;
-              this.subscription.sendUserID(data.userID);
-            }
           }
           if (JSON.stringify(data).includes('roomID')) {
             console.log('send roomID');
@@ -64,26 +51,28 @@ export class GameOnService {
             }
           }
 
+          if (data.costProject !== undefined) {
+            this.costProject = data.costProject;
+          }
+
+          if (data.delayProject !== undefined) {
+            this.delayProject = data.delayProject;
+          }
+
+          if (data.failureProject !== undefined) {
+            this.failureProject = data.failureProject;
+          }
 
           if (data.response === 'LAUNCH_GAME') {
+
+
+            if (data.player.userID !== undefined) {
+              console.log('send userID');
+              this.userID = data.player.userID;
+              this.subscription.sendUserID(data.player.userID);
+            }
+
             console.log(data);
-            const cost = {
-              minCost: data.project.minCost,
-              maxCost: data.project.maxCost
-            };
-            this.subscription.sendCosts(cost);
-
-            const days = {
-              minTime: data.project.minTime,
-              maxTime: data.project.maxTime
-            };
-            this.subscription.sendDays(days);
-
-            const failure = {
-              minFailure: data.project.minFailure,
-              maxFailure: data.project.maxFailure
-            };
-            this.subscription.sendCosts(failure);
             this.subscription.sendActivities(data.activities);
             this.currentActivityID = data.currentActivityID;
             console.log('after++++++++++++  ' + data.activities);
@@ -102,40 +91,12 @@ export class GameOnService {
             this.currentActivity = this.currentStep[currentId - 1];
             console.log(this.currentActivity);
             this.subscription.sendCurrentActivity(this.currentActivity);
-            if (data.costProject !== undefined) {
-              this.costProject = data.costProject;
-            }
-
-            if (data.delayProject !== undefined) {
-              this.delayProject = data.delayProject;
-            }
-
-            if (data.failureProject !== undefined) {
-              this.failureProject = data.failureProject;
-            }
           }
 
           if (data.response === 'UPDATE_PAYMENT') {
             const currentId = data.activityID;
             this.currentStep[currentId - 1].history = data;
             console.log(this.currentStep[currentId - 1].history);
-            const cost = {
-              minCost: data.project.minCost,
-              maxCost: data.project.maxCost
-            };
-            this.subscription.sendCosts(cost);
-
-            const days = {
-              minTime: data.project.minTime,
-              maxTime: data.project.maxTime
-            };
-            this.subscription.sendDays(days);
-
-            const failure = {
-              minFailure: data.project.minFailure,
-              maxFailure: data.project.maxFailure
-            };
-            this.subscription.sendFailures(failure);
           }
           console.log(data);
           return data;

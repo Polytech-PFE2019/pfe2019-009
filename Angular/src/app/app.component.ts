acb015fe810971990581cba66f8efc33ecf166c8
WANGWei <wei.wang@etu.unice.fr>
2019-12-06 10:21:45 +0100

    ajouté animation pour piocher la carte de risque

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -3,17 +3,21 @@ import {LobbyService} from './service/lobbyService/lobby.service';
 
 import {Component} from '@angular/core';
 import {SocketRequest} from '../Request';
+import {transition, trigger, useAnimation} from '@angular/animations';
+import {bounce, flipOutX} from 'ng-animate';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [WebsocketService, LobbyService]
+  providers: [WebsocketService, LobbyService],
 })
 export class AppComponent {
 
   test: SocketRequest = new SocketRequest();
   test1 = [];
+  flipOutX: any;
+  isVanished = false;
 
 
   constructor() {
@@ -29,10 +33,30 @@ export class AppComponent {
       negociationID: '179f781a-1be1-40df-882dacb48',
       response: 'START_NEGOTIATE'
     };
+
+    const jj = {
+      negociationID: '179f781a-1be1-40df-882dacb48',
+      response: 'START_NEGOTIATE'
+    };
+
+    const jjj = {
+      negociationID: '179f781a-1be1-40df-882dacb48',
+      response: 'START_NEGOTIATE'
+    };
     this.test1.push(i);
     this.test1.push(k);
     this.test1.push(j);
+    this.test1.push(jj);
+    this.test1.push(jjj);
   }
 
+
+  testFlip() {
+    if (this.flipOutX === null) {
+      this.flipOutX = true;
+    } else {
+      this.flipOutX = !this.flipOutX;
+    }
+  }
 
 }
--- Angular/src/app/commun-module/risk-back/risk-back.component.ts
+++ Angular/src/app/commun-module/risk-back/risk-back.component.ts
@@ -1,14 +1,21 @@
 import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {Risks} from '../../model/risks';
 import {SubscriptionService} from '../../service/subscriptionSerivce/subscription.service';
+import {transition, trigger, useAnimation} from '@angular/animations';
+import {flipOutX} from 'ng-animate';
 
 @Component({
   selector: 'app-risk-back',
   templateUrl: './risk-back.component.html',
-  styleUrls: ['./risk-back.component.css']
+  styleUrls: ['./risk-back.component.css'],
+  animations: [
+    trigger('flipOutX', [transition('* => *', useAnimation(flipOutX, {
+      // Set the duration to 5seconds and delay to 2seconds
+    }))])]
 })
 export class RiskBackComponent implements OnInit {
   @Input() step: number;
+  @Input() myTiming: any;
   @Output() sendCard = new EventEmitter();
   risks: any = Risks;
   random = 0;
@@ -26,14 +33,21 @@ export class RiskBackComponent implements OnInit {
     day: null,
     money: null,
   };
-  constructor(private subService: SubscriptionService) { }
+  flipOutX = false;
+  isVanished = true;
+  isDiabled = false;
+
+  constructor(private subService: SubscriptionService) {
+  }
+
   getRandomFromSon(event) {
     this.random = event;
     this.drawCard();
   }
+
   drawCard(): void {
     if (this.riskRemainNb !== 0) {
-      this.riskRemainNb --;
+      this.riskRemainNb--;
       this.cardNb++;
     }
     this.riskUnavailable = (this.riskRemainNb === 0);
@@ -46,19 +60,26 @@ export class RiskBackComponent implements OnInit {
   getId($event) {
     console.log($event);
     this.testid = $event;
-    const tmp = this.risks.filter(next => next.id === this.testid )[0];
-    console.log(this.risks.filter(next => next.id === this.testid )[0]);
-    this.test.id = this.risks.filter(next => next.id === this.testid )[0].id;
-    this.test.title = this.risks.filter(next => next.id === this.testid )[0].title;
-    this.test.risk = this.risks.filter(next => next.id === this.testid )[0].risk;
-    this.test.day = this.risks.filter(next => next.id === this.testid )[0].day;
-    this.test.money = this.risks.filter(next => next.id === this.testid )[0].money;
-    this.test.resource = this.risks.filter(next => next.id === this.testid )[0].resource;
+    const tmp = this.risks.filter(next => next.id === this.testid)[0];
+    console.log(this.risks.filter(next => next.id === this.testid)[0]);
+    this.test.id = this.risks.filter(next => next.id === this.testid)[0].id;
+    this.test.title = this.risks.filter(next => next.id === this.testid)[0].title;
+    this.test.risk = this.risks.filter(next => next.id === this.testid)[0].risk;
+    this.test.day = this.risks.filter(next => next.id === this.testid)[0].day;
+    this.test.money = this.risks.filter(next => next.id === this.testid)[0].money;
+    this.test.resource = this.risks.filter(next => next.id === this.testid)[0].resource;
     this.subService.sendHistory(this.test);
     console.log(this.test);
     console.log(this.subService.riskHistory);
   }
+
   ngOnInit() {
+    console.log(this.myTiming);
+    setTimeout(() => {
+      this.isVanished = false;
+      this.isDiabled = true;
+      console.log(this.isVanished);
+    }, this.myTiming * 1000 + 1000);
   }
 
 }

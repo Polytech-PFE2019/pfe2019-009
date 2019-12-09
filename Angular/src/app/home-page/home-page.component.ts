3e912a0e6858436c5dc31f92a44da3b007eab8ab
Gilles Chandelier <gilles.chandelier@etu.unice.fr>
2019-12-09 11:07:37 +0100

    add interval for displaying lobbys

--- Angular/src/app/home-page/home-page.component.ts
+++ Angular/src/app/home-page/home-page.component.ts
@@ -1,45 +1,48 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
-import {GameCreatorComponent} from '../modal-module/game-creator/game-creator.component';
-import {RoomService} from '../service/roomService/room.service';
-import {SocketRequest} from '../../Request';
-import {LobbyService} from '../service/lobbyService/lobby.service';
-import {RoomsService} from '../rooms.service';
-import {Rooms} from '../rooms';
-import {Router} from '@angular/router';
-import {Globals} from '../globals';
-import {NzMessageService} from 'ng-zorro-antd';
-import {SubscriptionService} from '../service/subscriptionSerivce/subscription.service';
-import {Subscription} from 'rxjs';
-import {HttpParams} from "@angular/common/http";
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
+import { GameCreatorComponent } from '../modal-module/game-creator/game-creator.component';
+import { RoomService } from '../service/roomService/room.service';
+import { SocketRequest } from '../../Request';
+import { LobbyService } from '../service/lobbyService/lobby.service';
+import { RoomsService } from '../rooms.service';
+import { Rooms } from '../rooms';
+import { Router } from '@angular/router';
+import { Globals } from '../globals';
+import { NzMessageService } from 'ng-zorro-antd';
+import { SubscriptionService } from '../service/subscriptionSerivce/subscription.service';
+import { Subscription } from 'rxjs';
+import { HttpParams } from "@angular/common/http";
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent implements OnInit {
+export class HomePageComponent implements OnInit, OnDestroy {
 
-  @ViewChild(GameCreatorComponent, {static: true})
+  @ViewChild(GameCreatorComponent, { static: true })
   gameCreator;
   value = '';
   username = '';
   listOfDisplayData: Rooms[] = [];
   roomID: any;
   userName: string;
+  lobbyIntervalID;
 
   constructor(private roomsService: RoomsService,
-              private globals: Globals,
-              private router: Router,
-              private subscriptionService: SubscriptionService,
-              private message: NzMessageService,
-              private lobbyService: LobbyService) {
+    private globals: Globals,
+    private router: Router,
+    private subscriptionService: SubscriptionService,
+    private message: NzMessageService,
+    private lobbyService: LobbyService) {
   }
 
   ngOnInit() {
-    this.roomsService.getRoomsInfos().subscribe(data => {
-      console.log(data);
-      this.listOfDisplayData = data;
-    });
+    this.getAllLobby();
+
+    this.lobbyIntervalID = setInterval(() => {
+      this.getAllLobby();
+    }, 5000
+    )
 
     this.lobbyService.messages.subscribe(data => {
       console.log(data);
@@ -49,6 +52,13 @@ export class HomePageComponent implements OnInit {
     });
 
 
+  }
+
+  getAllLobby() {
+    this.roomsService.getRoomsInfos().subscribe(data => {
+      console.log(data);
+      this.listOfDisplayData = data;
+    });
   }
 
   createASalon() {
@@ -93,5 +103,9 @@ export class HomePageComponent implements OnInit {
     } else {
       return true;
     }
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.lobbyIntervalID);
   }
 }

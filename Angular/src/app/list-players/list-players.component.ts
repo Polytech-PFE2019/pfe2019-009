3ac1a264de9449a27c3f8802f32ae9b7974bea4c
LiuJiaqi-unice <katherine9677@163.com>
2019-12-12 14:22:53 +0100

    #18 Liste d'histoire de chaque joueur

--- Angular/src/app/list-players/list-players.component.ts
+++ Angular/src/app/list-players/list-players.component.ts
@@ -1,5 +1,6 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SubscriptionService} from "../service/subscriptionSerivce/subscription.service";
+import {History} from "../model/history";
 
 @Component({
   selector: 'app-list-players',
@@ -8,12 +9,84 @@ import {SubscriptionService} from "../service/subscriptionSerivce/subscription.s
 })
 export class ListPlayersComponent implements OnInit {
   @Output() close = new EventEmitter();
+  // @Input() getHistory = [];
+  getHistory: any;
   roles: any[] = [];
+  histories: History[] = [];
+  userID: any;
+  totalResourcePayment: number;
+  totalMoneyPayment: number;
+  payments:any[] =[];
 
   constructor(private subscription: SubscriptionService) { }
 
   ngOnInit() {
+    this.payments =[];
+    this.totalResourcePayment = 0;
+    this.totalMoneyPayment = 0;
+    this.histories = [];
+    this.getHistory = {
+      activityID: 1,
+      roleID: 1,
+      payments: [
+        {
+          amount: 3,
+          type: 'DAYS',
+          bonus: 2,
+          extraPaying: true,
+        },
+        {
+          amount: 2,
+          type: 'MANDATORY',
+          bonus: 0,
+          extraPaying: false,
+        },
+        ],
+    };
     this.roles = this.subscription.roles;
+    // this.userID = this.subscription.userId;
+    this.userID = 1;
+
+    if (this.getHistory.roleID === this.userID) {
+      for (const p of this.getHistory.payments){
+        this.totalResourcePayment += p.amount;
+        switch (p.type) {
+          case 'MANDATORY':
+            const basic = {
+              activity: 'Resource(s) obligatoire(s)',
+              amount: p.amount,
+              result: ''
+            };
+            this.payments.push(basic);
+            break;
+          case 'DAYS':
+            const day = {
+              activity: 'Durées',
+              amount: p.amount,
+              result: '-' + p.bonus + ' délai'
+            };
+            this.payments.push(day);
+            break;
+          case 'RISKS':
+            const risk = {
+              activity: 'Defaillances',
+              amount: p.amount,
+              result: '-' + p.bonus + ' risques'
+            };
+            this.payments.push(risk);
+            break;
+        }
+      }
+      const total = {
+        activity: 'Total',
+        amount: this.totalResourcePayment,
+      };
+      this.payments.push(total);
+      this.histories.push({
+        activityID: this.getHistory.activityID,
+        payments: this.payments,
+      });
+    }
   }
 
   closeList() {
